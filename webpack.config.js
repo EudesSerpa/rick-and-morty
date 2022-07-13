2fdef293306539c86366c6579de69bb9a05c0804
Eudes Serpa <serpagame@gmail.com>
2022-07-13 18:19:33 -0500

    Added optimization option to the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -44,4 +46,8 @@ module.exports = {
     }),
     new MiniCssExtractPlugin(),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
 };
